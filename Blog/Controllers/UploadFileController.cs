249cf27d4469773d25de24024103edd65cab1b11
Egor <giviruk@gmail.com>
2020-05-21 20:37:36 +0300

    add upload file

---
+++ Blog/Controllers/UploadFileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(IFormFile file)
+        {
+            return Ok();
+        }
+    }
+}
